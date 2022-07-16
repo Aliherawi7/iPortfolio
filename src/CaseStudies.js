c5c17950ae78bfec2f2099f187953346921a1a27
Aliherawi <ALiherawi7@gmail.com>
2022-07-16 14:14:52 +0430

    Case-Studies component inforation added

---
+++ src/CaseStudies.js
@@ -0,0 +1,36 @@
+const CaseStudies = [
+    {
+        image: {
+            address: "./image/1.png",
+            alt: "NetFlix clone"
+        },
+         appInfo: {
+            header: "Web Application",
+            title: "NetFlix clone",
+            body: "Lorem atujoweriu woeiur oweiur weoiru weoriu eoruytoi w"
+        }
+    },
+    {
+        image: {
+            address: "./image/2.png",
+            alt: "eShop store"
+        },
+        appInfo: {
+            header: "Web Application",
+            title: "eShop store",
+            body: "Lorem atujoweriu woeiur oweiur weoiru weoriu eoruytoi w"
+        }
+    },
+    {
+        image: {
+            address: "./image/3.png",
+            alt: "Resume Builder"
+        },
+        appInfo: {
+            header: "Web Application",
+            title: "Resume Builder",
+            body: "Lorem atujoweriu woeiur oweiur weoiru weoriu eoruytoi w"
+        }
+    }
+]
+export default CaseStudies;
