eab7e8027b8465ee6d94d80cd19671e5622b2b67
Aliherawi <ALiherawi7@gmail.com>
2022-07-16 14:16:36 +0430

    slider desktop mode styles added

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,20 +1,34 @@
 import React from 'react'
+import "./Slider.css"
 
-function Slider1() {
-  return (
-    <div className='slider'>
-        <img src='' />
-        
-    </div>
-  )
-}
-function Slider2() {
+function Slider1({ appInfo, image }) {
     return (
-      <div className='slider'>
-  
-      </div>
+        <div className='slider'>
+            <img src={image.address} className="slider-image" alt={image.alt}/>
+            <div className='slider-text'>
+                <h3 className='slider-header-2'>{appInfo.header}</h3>
+                <h2 className='slider-header-1'>{appInfo.title}</h2>
+                <p className='slider-text-body'>
+                    {appInfo.body}
+                </p>
+            </div>
+        </div>
     )
 }
-const Slider =[Slider1, Slider2];
+function Slider2({ appInfo, image }) {
+    return (
+        <div className='slider'>
+            <img src={image.address} className="slider-image" alt={image.alt}/>
+            <div className='slider-text'>
+                <h3 className='slider-header-2'>{appInfo.header}</h3>
+                <h2 className='slider-header-1'>{appInfo.title}</h2>
+                <p className='slider-text-body'>
+                    {appInfo.body}
+                </p>
+            </div>
+        </div>
+    )
+}
+const Slider = [Slider1, Slider2];
 
 export default Slider
