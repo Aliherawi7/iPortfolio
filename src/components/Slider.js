b5366d94b276038cb96d87ff7df8e787abac136d
Aliherawi <ALiherawi7@gmail.com>
2022-07-16 12:47:58 +0430

    Slider component created for case studies at home component

---
+++ src/components/Slider.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Slider1() {
+  return (
+    <div className='slider'>
+        <img src='' />
+        
+    </div>
+  )
+}
+function Slider2() {
+    return (
+      <div className='slider'>
+  
+      </div>
+    )
+}
+const Slider =[Slider1, Slider2];
+
+export default Slider
