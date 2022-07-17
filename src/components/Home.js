9fa09ecd11907d309ee4dbbeac9256334132fa79
Aliherawi <ALiherawi7@gmail.com>
2022-07-17 23:03:30 +0430

    unused variable removed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from "react-router-dom"
 import "./Home.css"
 import Slider from './Slider'
-let counter = 0
+
 function Home() {
     
-
     return (
         <main className='home'>
             <div className='introduction padding-LR-90'>
