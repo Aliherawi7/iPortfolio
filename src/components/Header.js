f7291293291078407fc62d1c817268f201eab1f4
Aliherawi <ALiherawi7@gmail.com>
2022-07-22 23:03:24 +0430

    active property added for each link with functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./Header.css"
 
 function Header() {
+    //this state is for activing the header link
+    const [path, setPath] = useState(window.location.pathname)
+    const setActive = (path)=>{
+        setPath(path)
+    }
     return (
         <header className='header padding-LR-90'>
             <Link to="/">
                 <img src="./image/AH1.png" className='nav-brand' />
             </Link>
             <nav>
-                <Link to={"/"}>Home</Link>
-                <Link to={"/works"}>Works</Link>
-                <Link to={"/blog"}>Blog</Link>
+                <Link to={"/"} onClick={()=>setActive("/")}>
+                    <span className={path=="/" ? "active":""}>Home</span>
+                </Link>
+                <Link to={"/works"} onClick={()=>setActive("/works")}>
+                    <span className={path=="/works" ? "active":""}>Works</span>
+                </Link>
+                <Link to={"/blog"} onClick={()=>setActive("/blog")}>
+                    <span className={path=="/blog" ? "active":""}>Blog</span>
+                </Link>
                 <button className='hire-me'>
                     Hire Me!
                 </button>
