766011a649b18d398e7f2d17e1d2ee1932a34fa9
Aliherawi <ALiherawi7@gmail.com>
2022-07-22 23:04:44 +0430

    wroks carted style and key react property added for each work card

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -7,17 +7,20 @@ function Works() {
 
   return (
     <div className='works margin-LR-90'>
-        <h2>My Works</h2>
+        <h2 className='works-title'>My Works</h2>
         <div className='works-container '>
             {CaseStudies.map(work => {
                 return(
-                <div className='work'>
+                    
+                <div className='work' key={work.id}>
+                    <Link to={"/works/"+work.id}>
                     <div className='image-container'>
                         <img src={work.image.address} alt={work.image.alt}/>
                     </div>
+                    </Link>
                     <div className='work-tool'>
                         <h2>{work.appInfo.title}</h2>
-                        <Link to="">Know More</Link>
+                        <Link to={"/works/"+work.id}>Know More</Link>
                     </div>
                 </div>)
             })}
