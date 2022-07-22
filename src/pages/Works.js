77ba507f2da4df5ff54d3821734cc8fc27cff713
Aliherawi <ALiherawi7@gmail.com>
2022-07-22 22:05:43 +0430

    works card added and styled

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import "./Works.css"
 import CaseStudies from '../CaseStudies'
+import {Link} from "react-router-dom"
 
 function Works() {
 
   return (
-    <div className='works'>
+    <div className='works margin-LR-90'>
         <h2>My Works</h2>
-        <div className='works-container'>
+        <div className='works-container '>
             {CaseStudies.map(work => {
-                return(<div className='work'>
+                return(
+                <div className='work'>
                     <div className='image-container'>
-                        <img src={work.image}/>
+                        <img src={work.image.address} alt={work.image.alt}/>
                     </div>
                     <div className='work-tool'>
                         <h2>{work.appInfo.title}</h2>
