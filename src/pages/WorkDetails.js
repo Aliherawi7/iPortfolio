95886f9d530f78c3e21a18337128a7676b4ea15f
Aliherawi <ALiherawi7@gmail.com>
2022-07-23 23:42:11 +0430

    WorkDetail component created and initialized

---
+++ src/pages/WorkDetails.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import CaseStudies from '../CaseStudies';
+import "./WorkDetails.css"
+
+function WorkDetails() {
+    const { id } = useParams();
+    const work = CaseStudies[id];
+    console.log(work)
+    return (
+        <div className='work-details'>
+            <h2>{work.appInfo.title}</h2>
+            <div className='imgae-container'>
+                <img src={work.image.address} alt={work.image.alt} />
+            </div>
+            <div className='details-text'>
+                <div className='detail-body'>
+                    <h3>Overview</h3>
+                    <p>
+                        {work.appInfo.body}
+                    </p>
+                </div>
+                <div className='detail-body'>
+                    <h3>Problem</h3>
+                    <p>
+                        {work.appInfo.body}
+                    </p>
+                </div>
+                <div className='detail-body'>
+                    <h3>Solution</h3>
+                    <p>
+                        {work.appInfo.body}
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WorkDetails
