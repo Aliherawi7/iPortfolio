a8549992716082bfd60d926e749c9fe0068187be
Aliherawi <ALiherawi7@gmail.com>
2022-07-26 14:25:03 +0430

    Blog  page created

---
+++ src/pages/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Blog.css"
+
+function Blog() {
+  return (
+    <div className='blog right-to-left'>
+        Blog page
+    </div>
+  )
+}
+
+export default Blog
