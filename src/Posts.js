535a0eadc4be6fb338bb36ab577e92a6e0c9ad9d
Aliherawi <ALiherawi7@gmail.com>
2022-07-29 23:47:33 +0430

    static post object created for test

---
+++ src/Posts.js
@@ -0,0 +1,55 @@
+const posts = [
+    {
+        id:0,
+        title: "Spring boot",
+        pictureUrl: "/image/blog/spring-boot.png",
+        author: "Ali Herawi",
+        date: "2022-2-12",
+        timeout: "8 minutes",
+        comment: [{
+            username: "James hart",
+            date: "2022-3-4",
+            comment: "thank you man its really helpful. please continue....",
+            pictureUrl: "image/users/jame.png"
+        }],
+        postBody: {
+            text: "Spring framework is a huge environment for building enterprise application",
+        }
+    },
+    {
+        id:1,
+        title: "React js",
+        pictureUrl: "/image/blog/react.png",
+        author: "Ali Herawi",
+        date: "2022-2-12",
+        timeout: "8 minutes",
+        comment: [{
+            username: "James hart",
+            date: "2022-3-4",
+            comment: "thank you man its really helpful. please continue....",
+            pictureUrl: "image/users/jame.png"
+        }],
+        postBody: {
+            text: "React Js library or api is a huge environment for building web and mobile application",
+        }
+    },
+    {
+        id:2,
+        title: "Microservices",
+        pictureUrl: "/image/blog/spring-microservices.png",
+        author: "Ali Herawi",
+        date: "2022-2-12",
+        timeout: "8 minutes",
+        comment: [{
+            username: "James hart",
+            date: "2022-3-4",
+            comment: "thank you man its really helpful. please continue....",
+            pictureUrl: "image/users/jame.png"
+        }],
+        postBody: {
+            text: "microservices are the common subject on spring boot framwork learning",
+        }
+    }
+]
+
+export default posts
