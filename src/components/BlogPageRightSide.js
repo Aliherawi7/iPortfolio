adb270e4db23e6e5cb2c8556d792a1b15268aa3c
Aliherawi <ALiherawi7@gmail.com>
2022-07-29 23:48:13 +0430

    clasName bug fixed

--- src/components/BlogPageRightSide.js
+++ src/components/BlogPageRightSide.js
@@ -5,18 +5,18 @@ import "./BlogPageRightSide.css"
 function BlogPageRightSide() {
   return (
     <div className='right-side'>
-        <aside class="single_sidebar_widget search_widget">
+        <aside className="single_sidebar_widget search_widget">
             <form action="#">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search Keyword" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Keyword'" />
-                <i class="bi bi-search"></i>
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Search Keyword" onFocus="this.placeholder = ''" onBlur="this.placeholder = 'Search Keyword'" />
+                <i className="bi bi-search"></i>
               </div>
-              <button class="search-btn" type="submit">Search</button>
+              <button className="search-btn" type="submit">Search</button>
             </form>
           </aside>
-          <aside class="single_sidebar_widget post_category_widget">
-            <h4 class="widget_title">Category</h4>
-            <ul class="list cat-list">
+          <aside className="single_sidebar_widget post_category_widget">
+            <h4 className="widget_title">Category</h4>
+            <ul className="list cat-list">
               <li>
                 <Link to={''}>
                   <p>Java</p>
@@ -49,38 +49,38 @@ function BlogPageRightSide() {
               </li>
             </ul>
           </aside>
-          <aside class="single_sidebar_widget popular_post_widget">
-            <h3 class="widget_title">Recent Post</h3>
-            <div class="media post_item">
+          <aside className="single_sidebar_widget popular_post_widget">
+            <h3 className="widget_title">Recent Post</h3>
+            <div className="media post_item">
               <img src="/image/post/java.png" alt="post" />
-              <div class="media-body">
+              <div className="media-body">
                 <Link to={''}>
                   <h3>Binary search in java</h3>
                 </Link>
                 <p>January 12, 2019</p>
               </div>
             </div>
-            <div class="media post_item">
+            <div className="media post_item">
               <img src="/image/post/spring-data-jpa.png" alt="post" />
-              <div class="media-body">
+              <div className="media-body">
                 <Link to={''}>
                   <h3>Spring data jpa</h3>
                 </Link>
                 <p>02 Hours ago</p>
               </div>
             </div>
-            <div class="media post_item">
+            <div className="media post_item">
               <img src="/image/post/java.png"alt="post" />
-              <div class="media-body">
+              <div className="media-body">
                 <Link to={''}>
                   <h3>React context api</h3>
                 </Link>
                 <p>03 Hours ago</p>
               </div>
             </div>
-            <div class="media post_item">
+            <div className="media post_item">
               <img src="/image/post/java.png" alt="post" />
-              <div class="media-body">
+              <div className="media-body">
                 <Link to={''}>
                   <h3>Arrays in Javascript</h3>
                 </Link>
