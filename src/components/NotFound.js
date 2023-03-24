4c8c7baa92db42461ac10784f437e87bc5a05534
ali herawi <aliherawi7@gmail.com>
2023-03-24 15:36:05 -0700

    not FOund page

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./NotFound.css"
+
+function NotFound() {
+    return (
+        <div className='not_found fade-in'>
+            <h2>404</h2>
+            <p className='title'>Ooops... resource not found</p>
+        </div>
+    )
+}
+
+export default NotFound
