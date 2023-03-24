63d60f1ada2577543959028219697172d37f1cbe
ali herawi <aliherawi7@gmail.com>
2023-03-24 14:58:36 -0700

    NotFound pge added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Loading from "./components/Loading"
+import NotFound from './components/NotFound';
 
 const Header = React.lazy(() => import('./components/Header'));
 const Footer = React.lazy(() => import('./components/Footer'));
 const Home = React.lazy(() => import('./pages/Home'));
 const Works = React.lazy(() => import('./pages/Works'));
 const WorkDetails = React.lazy(() => import('./pages/WorkDetails'));
-const Blog = React.lazy(()=> import('./pages/Blog'));
-const Post = React.lazy(()=> import('./pages/Post'));
+const Blog = React.lazy(() => import('./pages/Blog'));
+const Post = React.lazy(() => import('./pages/Post'));
 
 
 function App() {
@@ -21,9 +22,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/works/:id" element={<WorkDetails />} />
           <Route path="/works" element={<Works />} />
-          <Route path="/blog" element={<Blog />}/>
-          <Route path="/blog/post:id" element={<Post />}/>
-          <Route path="*" element={<Loading />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/post:id" element={<Post />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
