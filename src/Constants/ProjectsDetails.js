4c09f74b151ba72b47bffecfbd9bcbce53a52476
ali herawi <aliherawi7@gmail.com>
2023-03-24 15:03:56 -0700

    this folder is going to hold the static data

---
+++ src/Constants/ProjectsDetails.js
@@ -0,0 +1,64 @@
+const ProjectDetails = [
+    {
+        id: 0,
+        image: {
+            address: "/image/1.png",
+            alt: "Sigma app"
+        },
+        appInfo: {
+            header: "Social Network",
+            title: "Sigma app",
+            body: "resume-builder has been developed and built with React (v17.0.2), JavaScript, jspdf(^2.5.1), kendo-react-pdf(^5.3.0) and React Router v6. For Styles, is used CSS."
+        }
+    },
+    {
+        id: 1,
+        image: {
+            address: "/image/2.png",
+            alt: "eShop store"
+        },
+        appInfo: {
+            header: "E-Commerce Web Application",
+            title: "eShop store",
+            body: "Proin laoreet elementum ligula, ac tincidunt lorem accumsan nec. Fusce eget urna ante. Donec massa velit, varius a accumsan ac, tempor iaculis massa. Sed placerat justo sed libero varius vulputate. Ut a mi tempus mass Sed eleifend sed nibh nec fringilla. Donec eu cursus sem, vitae tristique ante. Cras pretium rutrum egestas. Integer ultrices libero sed justo vehicula, eget"
+        }
+    },
+    {
+        id: 2,
+        image: {
+            address: "/image/3.png",
+            alt: "Resume Builder"
+        },
+        appInfo: {
+            header: "Web Application",
+            title: "Resume Builder",
+            body: "resume-builder has been developed and built with React v17.0.2, JavaScript, jspdf v2.5.1, kendo-react-pdf v5.3.0 and React Router v6. For Styling I used pure Css3 and for Bootsrap-icons "
+        }
+    },
+    {
+        id: 3,
+        image: {
+            address: "/image/4.png",
+            alt: "Music Player"
+        },
+        appInfo: {
+            header: "Web Application",
+            title: "Music Player",
+            body: "Proin laoreet elementum ligula, ac tincidunt lorem accumsan nec. Fusce eget urna ante. Donec massa velit, varius a accumsan ac, tempor iaculis massa. Sed placerat justo sed libero varius vulputate. Ut a mi tempus mass Sed eleifend sed nibh nec fringilla. Donec eu cursus sem, vitae tristique ante. Cras pretium rutrum egestas. Integer ultrices libero sed justo vehicula, eget"
+        }
+    },
+    {
+        id: 4,
+        image: {
+            address: "/image/5.png",
+            alt: "NetFlix clone"
+        },
+        appInfo: {
+            header: "Web Application",
+            title: "NetFlix clone",
+            body: "Proin laoreet elementum ligula, ac tincidunt lorem accumsan nec. Fusce eget urna ante. Donec massa velit, varius a accumsan ac, tempor iaculis massa. Sed placerat justo sed libero varius vulputate. Ut a mi tempus mass Sed eleifend sed nibh nec fringilla. Donec eu cursus sem, vitae tristique ante. Cras pretium rutrum egestas. Integer ultrices libero sed justo vehicula, eget"
+        }
+    },
+]
+export default ProjectDetails;
+
