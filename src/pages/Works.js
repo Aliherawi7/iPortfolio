c8f05cf9d5755b1e5578b2fda5c0c113235c9d10
ali herawi <aliherawi7@gmail.com>
2023-03-24 15:28:54 -0700

    projectDetails added as caseStudies

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,6 +1,6 @@
 import React,{useEffect} from 'react'
 import "./Works.css"
-import CaseStudies from '../CaseStudies'
+import ProjectDetails from '../Constants/ProjectsDetails'
 import { Link } from "react-router-dom"
 
 function Works() {
@@ -11,7 +11,7 @@ function Works() {
         <div className='works margin-LR-90 fade-in'>
             <h2 className='works-title'>My Works</h2>
             <div className='works-container '>
-                {CaseStudies.map(work => {
+                {ProjectDetails.map(work => {
                     return (
                         <div className='work' key={work.id}>
                             <Link to={"/works/" + work.id}>
