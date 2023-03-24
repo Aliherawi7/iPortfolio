9f4984b47bb92c00ef4f7e3bdaefa3d10ed9c105
ali herawi <aliherawi7@gmail.com>
2023-03-24 15:30:28 -0700

    main color chaged, skills and about me  sections added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,39 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Link, useNavigate } from "react-router-dom"
 import { actions } from '../reducer';
 import { useStatevalue } from '../stateProvider';
 import "./Home.css"
-import Slider from '../components/Slider'
+import Slider from "../components/Slider"
+import ProjectDetails from '../Constants/ProjectsDetails';
+import Skills from '../Constants/Skills';
+import Skill from '../components/Skill';
+import SocialNetworks from '../components/SocialNetworks';
+import AboutMe from '../components/AboutMe';
+
 
 function Home() {
     const navigate = useNavigate();
     const [state, dispatch] = useStatevalue();
     useEffect(() => {
-        window.scrollTo(0,0);
-        
+        window.scrollTo(0, 0);
+
     }, [])
-    const seeWorks = ()=>{
+    const seeWorks = () => {
         dispatch({
             type: actions.ACTIVE_NAV_LINK,
-            item:"/works"
+            item: "/works"
         })
         navigate("./works");
     }
     return (
         <main className='home left-to-right'>
-            <div className='introduction'>
+            <div className='introduction diff_background'>
                 <div className='margin-LR-90'>
                     <div className='intro-text'>
                         <p>Hello, my name is</p>
                         <h1>Ali Herawi</h1>
-                        <h3>I'm fullstack developer. I make awesome web application with react js and spring boot.</h3>
+                        <h3>I'm fullstack developer. I make awesome web applications using react js and spring boot.</h3>
+                        <SocialNetworks size='large' />
                         <button className='see-my-works' onClick={seeWorks}>
                             See My Works
                         </button>
@@ -36,39 +43,28 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <div className='expert-in-container'>
+            <AboutMe />
+            <div className='skills_container diff_background section'>
                 <div className='margin-LR-90'>
-                    <h1>Expert in</h1>
-                    <div className='experts'>
-                        <div className='expert'>
-                            <i className='bi bi-server'></i>
-                            <h2>Web application</h2>
-                            <p>Lorem opwuiero iueroweiur oepiur poiu</p>
-                            <Link to="">view more</Link>
-                        </div>
-                        <div className='expert'>
-                            <i className='bi bi-laptop'></i>
-                            <h2>Web design</h2>
-                            <p>Lorem opwuiero iueroweiur oepiur poiu</p>
-                            <Link to="">view more</Link>
-                        </div>
-                        <div className='expert'>
-                            <i className='bi bi-tablet-landscape'></i>
-                            <h2>Web application</h2>
-                            <p>Lorem opwuiero iueroweiur oepiur poiu</p>
-                            <Link to="">view more</Link>
-                        </div>
+                    <h1 className='section_title'>Skills</h1>
+                    <div className='skills'>
+                        {Skills.map(item => {
+                            return (
+                                <Skill icon={item.icon} name={item.name} color={item.color} level={item.level} />
+                            )
+                        })}
                     </div>
                 </div>
             </div>
-            <div className='case-studies '>
-                <h2 className=' margin-LR-90'>Case Studies</h2>
-                <Slider />
+            <div className='case-studies diff_background section'>
+                <h2 className='section_title margin-LR-90'>Projects</h2>
+                <Slider products={ProjectDetails} delay={5} />
             </div>
-            <div className='hire-me-box'>
+
+            <div className='hire-me-box section'>
                 <div className='margin-LR-90'>
                     <div className='hire-me-text'>
-                        <h2>Make It Awesome</h2>
+                        <h2 className='section_title'>Make It Awesome</h2>
                         <p>
                             Let's meke something more meaningful and make things more vitual conceptual and packed with a very user friendly
                         </p>
