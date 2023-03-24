0b7e6d298d1538737388be9e0ed7ba72c3263236
ali herawi <aliherawi7@gmail.com>
2023-03-24 15:26:02 -0700

    loading component completely updated

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import './Loading.css'
+import "./Loading.css"
 
 function Loading() {
-    return (
-        <div className="spinner-wrapper" >
-            <div className="spinner">
-                <div className="bounce1"></div>
-                <div className="bounce2"></div>
-                <div className="bounce3"></div>
-            </div>
-        </div>
-    ) 
+  return (
+    <div className="spinner__wrapper">
+        <div className="spinner"></div>
+    </div>
+  )
 }
 
 export default Loading
