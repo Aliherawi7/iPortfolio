a39c8d605c110083aee4525072add3e7e04236e6
ali herawi <aliherawi7@gmail.com>
2023-03-24 15:24:58 -0700

    contact_me section seprated as a component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import "./Footer.css"
+import SocialNetworks from './SocialNetworks'
 
 
 function Footer() {
     return (
         <footer >
             <div className='footer-container margin-LR-90'>
-                <p>© 2022 - Ali Herawi</p>
-                <div className='contact-me'>
-                    <a href=''><i className='bi bi-linkedin'></i></a>
-                    <a href=''><i className='bi bi-twitter'></i></a>
-                    <a href=''><i className='bi bi-github'></i></a>
-                    <a href=''><i className='bi bi-instagram'></i></a>
-                </div>
+                <p>© {new Date().getUTCFullYear()} - Ali Herawi</p>
+                <SocialNetworks />
             </div>
 
         </footer>
