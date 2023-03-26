c76244471982a5e3fbdf1e51ce74b460b2fff4ec
ali herawi <aliherawi7@gmail.com>
2023-03-26 01:04:02 -0700

    backgournd and font styles updated

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
 import "./Slider.css"
-import ProjectDetails from "../Constants/ProjectsDetails"
 import { Link } from "react-router-dom"
 
-function Slider({products, delay = 8,}) {
-    let [product, setProduct] = useState(ProjectDetails);
+function Slider({ products = [], delay = 8, }) {
+    let [product, setProduct] = useState(products);
     let counter = 0
     const slides = useRef();
     useEffect(() => {
@@ -22,7 +21,7 @@ function Slider({products, delay = 8,}) {
         if (counter <= (-(products.length) * 100)) {
             counter = 0
             slides.current.style.transition = "transform 0s ease";
-        }else{
+        } else {
             slides.current.style.transition = "transform 2s cubic-bezier(0.71, 0.15, 0.3, 0.81)";
         }
         slides.current.style.transform = `translateX(${counter}vw)`;
@@ -32,7 +31,7 @@ function Slider({products, delay = 8,}) {
         if (counter >= 100) {
             counter = -(products.length) * 100
             slides.current.style.transition = "transform 0s ease";
-        }else{
+        } else {
             slides.current.style.transition = "transform 2s cubic-bezier(0.71, 0.15, 0.3, 0.81)";
         }
         slides.current.style.transform = `translateX(${counter}vw)`;
@@ -50,7 +49,7 @@ function Slider({products, delay = 8,}) {
                                     <div className='image-info'>
                                         <h1 className='slider-header-2'>{item?.appInfo.title}</h1>
                                         <h2 className='slider-header-1'>{item?.appInfo.header}</h2>
-                                        <Link to={'/works/' + item?.id}>more details</Link>
+                                        <Link to={'/works/' + item?.id} className="btn_custom">more details</Link>
                                     </div>
                                 </div>
                             )
