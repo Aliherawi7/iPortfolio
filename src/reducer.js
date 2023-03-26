e374676d0dcc7a3da17b9e89ba191f0736867c3f
ali herawi <aliherawi7@gmail.com>
2023-03-26 01:06:36 -0700

    logs removed

--- src/reducer.js
+++ src/reducer.js
@@ -2,14 +2,12 @@ export const initialState = {
     navActive: window.location.pathname
 }
 export const actions = {
-    ACTIVE_NAV_LINK : "active_nav_link"
+    ACTIVE_NAV_LINK: "active_nav_link"
 }
 
 const reducer = (state, action) => {
-    console.log(action, "in reducer fir")
-    switch (action.type){
-        case actions.ACTIVE_NAV_LINK:{
-            console.log(action.item,"in reducer")
+    switch (action.type) {
+        case actions.ACTIVE_NAV_LINK: {
             return {
                 ...state,
                 navActive: action.item
