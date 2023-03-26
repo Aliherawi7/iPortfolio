2a9f2b472ecec647960285c477e99438fc1a32ac
ali herawi <aliherawi7@gmail.com>
2023-03-26 01:02:40 -0700

    instagram link added

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,18 +5,18 @@ function AboutMe() {
     return (
         <div className='about_me padding-LR-90 section'>
             <h2 className='section_title'>About</h2>
-            <div class="list_container">
+            <div className="list_container">
                 <ul>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Full name:</strong> <span>Ali Ahmad Herawi</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>10 Mar 2000</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+93 797-608-705</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Location:</strong> <span>Herat, Afghanistan</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Full name:</strong> <span>Ali Ahmad Herawi</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>10 Mar 2000</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+93 797-608-705</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Location:</strong> <span>Herat, Afghanistan</span></li>
                 </ul>
                 <ul>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>{new Date().getFullYear() - 2000}</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Bachelor of Science</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>Aliherawi7@gmail.com</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Age:</strong> <span>{new Date().getFullYear() - 2000}</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Bachelor of Science</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Email:</strong> <span>Aliherawi7@gmail.com</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
                 </ul>
             </div>
         </div>
