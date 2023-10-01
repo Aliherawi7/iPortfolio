6260abadd96e2857745434ecba5381a3c8fb481b
ali herawi <aliherawi7@gmail.com>
2023-10-01 12:41:28 +0430

    resume edu and exp added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -77,6 +77,87 @@ function Home() {
                     </div>
                 </div>
             </div>
+            <div className='resume padding-LR-90 section'>
+                <div className="resume-section">
+                    <h3 className='section_title'>Professional Experience</h3>
+                    <div className="resume-item">
+                        <h4>Afghan Web, Herat - Software Developer</h4>
+                        <h5>July 2021 - May 2023</h5>
+                        <p><em>Key responsibilities:</em></p>
+                        <ul>
+                            <li>Designed, developed, and maintained software applications
+                                using various technologies.</li>
+                            <li>Collaborated with clients to understand their requirements and
+                                deliver tailored solutions.
+                            </li>
+                            <li>Conducted thorough testing and debugging to ensure
+                                high-quality and bug-free software.</li>
+                            <li>Provided technical support and troubleshooting for deployed
+                                applications.
+                            </li>
+                        </ul>
+                        <p><em>Notable accomplishments:</em></p>
+                        <ul>
+                            <li>ntegrated continuous integration and deployment (CI/CD)
+                                pipelines, resulting in faster and more reliable software
+                                releases
+                            </li>
+                            <li>Developed an automated testing framework, reducing manual
+                                testing efforts by 60% and improving software quality.
+                            </li>
+                            <li>Collaborated with the team to identify and resolve software
+                                defects, ensuring high-quality deliverables.
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="resume-item">
+                        <h4>Netlinks, Herat - Backend Developer</h4>
+                        <h5>March 2019 - June 2021</h5>
+                        <p><em>Key responsibilities:</em></p>
+                        <ul>
+                            <li>Developed and maintained backend systems, ensuring
+                                scalability and performance.</li>
+                            <li>Collaborated with cross-functional teams to gather
+                                requirements and implement solutions.
+                            </li>
+                            <li>Integrated third-party APIs for seamless data exchange.</li>
+                            <li>Conducted code reviews and implemented best practices for
+                                code quality and security.
+                            </li>
+                        </ul>
+
+                        <p><em>Notable accomplishments:</em></p>
+                        <ul>
+                            <li>Optimized database queries, reducing response time by 30%
+                                and improving overall system efficiency.</li>
+                            <li>Implemented a caching mechanism, resulting in a 40%
+                                decrease in server load and improved application
+                                responsiveness.
+                            </li>
+                            <li>Designed and implemented a RESTful API, enabling
+                                seamless integration with external systems and increasing
+                                data accessibility.
+                            </li>
+                            <li>Developed a robust error handling system, reducing system
+                                downtime by 20% and enhancing user experience.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="resume-section">
+                    <h3 className='section_title'>Education</h3>
+                    <div className="resume-item">
+                        <h4>Herat University, Herat — Bachelor of science in Computer science</h4>
+                        <h5>2015 - 2016</h5>
+                        <p>Studied a comprehensive curriculum in computer science, gaining a strong foundation in programming, algorithms, data structures, and software development. Developed problem-solving and analytical skills through coursework and projects.</p>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+
             <div className='case-studies diff_background section'>
                 <h2 className='section_title margin-LR-90'>Projects</h2>
                 <Slider products={ProjectsDetail} delay={5} />
@@ -93,7 +174,7 @@ function Home() {
                     <a href='https://linkedin.com/in/ali-herawi' className='btn_custom'>Hire Me!</a>
                 </div>
             </div>
-        </main>
+        </main >
     )
 }
 
